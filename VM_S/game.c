ae2e9800dc0b60684152d1a96abfb2aa56691f37
Flint Louis <flintlouis@Flints-MBP.lan>
2019-12-20 13:42:27 +0100

    get bytes added

--- VM_S/game.c
+++ VM_S/game.c
@@ -1,9 +1,23 @@
 #include "vm.h"
 
-void			play_game(t_vm *vm)
+/*
+** EXAMPLES:
+** int x = get_2bytes(&ARENA[i]); index vanaf waar je de bytes wilt lezen, geeft een integer terug in kleine endian!
+** int y = get_4bytes(&ARENA[i]); index vanaf waar je de bytes wilt lezen, geeft een integer terug in kleine endian!
+*/
+
+int			get_2bytes(t_byte *arena)
 {
-	while (GAME->cycles < GAME->cycles_to_die)
-	{
-		GAME->cycles = (GAME->cycles + 1) % GAME->cycles_to_die;
-	}
+	short b;
+
+	ft_memcpy(&b, arena, 2);
+	return (swap_16(b));
+}
+
+int			get_4bytes(t_byte *arena)
+{
+	int b;
+
+	ft_memcpy(&b, arena, 4);
+	return (swap_32(b));
 }
--- VM_S/main.c
+++ VM_S/main.c
@@ -24,5 +24,20 @@ int         main(int argc, char **argv)
 	retrieve_flags(vm, argc, argv);
 	check_champion_position(vm);
 	setup_game(vm);
+
+	/* DEBUG */
+	ft_putendl("");
+	int i = 0;
+	ft_printf("%s\n", g_op_tab[ARENA[i++]].name);
+	ft_printf("%d\n", ARENA[i++]);
+	ft_printf("%d\n", ARENA[i++]);
+	ft_printf("%d\n", get_2bytes(&ARENA[i]));
+	i += 2;
+	ft_printf("%d\n", get_2bytes(&ARENA[i]));
+	i += 2;
+	ft_printf("%s\n", g_op_tab[ARENA[i++]].name);
+	ft_printf("%d\n", get_4bytes(&ARENA[i]));
+
+
     return (0);
 }
