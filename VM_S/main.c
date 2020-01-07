e2da16b8435492e2ec78844ae575bb2189c9d7d0
Flint Hignett <fhignett@f0r3s15.codam.nl>
2020-01-07 16:44:29 +0100

    do_op added

--- VM_S/main.c
+++ VM_S/main.c
@@ -6,7 +6,7 @@
 /*   By: fhignett <fhignett@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/07 15:53:57 by fhignett       #+#    #+#                */
-/*   Updated: 2020/01/07 15:56:02 by fhignett      ########   odam.nl         */
+/*   Updated: 2020/01/07 16:44:24 by fhignett      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ static	t_vm	*init_vm(void)
 
 /*
 ** Reminder/to do:
-<<<<<<< HEAD
 ** - Basic operations
 ** - Add size to cursor->position after successful executed operation
 ** 		and set cursor->moved to true
@@ -40,15 +39,6 @@ static	t_vm	*init_vm(void)
 **		0 and 1, right?
 ** - function get_arena_index() added, might be usefull. Gives you the correct
 **		index back after saying how many steps
-=======
-** • Basic operations
-** • Add size to cursor-ßß>position after successful executed operation. And set cursor->moved to true
-** • Everytime live is performed GAME->live_counter++. Will be reset during the cursor live check.
-** • -d flag needs to be implemented, function already written.
-** • When doing an operation, when copying a value into the arena, watch out for max index, for example if
-**   	4 bytes are placed into the arena at index 4094, the last 2 bytes will be placed at index 0 and 1, right?
-** • function get_arena_index() added, might be usefull. Gives you the correct index back after saying how many steps
->>>>>>> 4f548b131133f2ffdab9e9f317ee223e000ec386
 **   	+ or - you want from current index.
 ** - ./corewar batman.cor buttman.s doesn't give an error?
 ** Visualizer if we have time/want to do it???
