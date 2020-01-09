a06a17a124cf6a78dff5ce7609cc8be9088c73a6
Robbe Van kets <rvan-ket@f1r2s21.codam.nl>
2020-01-09 17:14:58 +0100

    almost done w operations joepi

--- VM_S/operations.c
+++ VM_S/operations.c
@@ -54,7 +54,7 @@ void	st(t_vm *vm, t_cursor *c, t_arg *argument)
 	else if (arg2.type == 4)
 	{
 		value = swap_32(c->registry[arg1.value - 1]);
-		ft_memcpy(&ARENA[(c->position + arg2.value % IDX_MOD) % IDX_MOD], &value, 4);
+		put_value(ARENA, (c->position + arg2.value % IDX_MOD) % IDX_MOD, &value);
 		// swap_32();
 	}
 }
@@ -112,11 +112,11 @@ void	and(t_vm *vm, t_cursor *c, t_arg *argument)
 		if (argument[i].type == 2)
 			value[i] = argument[i].value;
 		if (argument[i].type == 4)
-			ft_memcpy(&value[i], &ARENA[c->position + argument[i].value % IDX_MOD], 4);		
+			value[i] = get_bytes(ARENA, c->position + argument[i].value % IDX_MOD, 4);
 		i++;
 	}
 	val = value[0] & value[1];
-	ft_memcpy(&c->registry[argument[2].value - 1], &val, 4);
+	c->registry[argument[2].value - 1] = val;
 	if (val == 0)
 		c->carry = 1;
 	else
@@ -137,10 +137,7 @@ void	or(t_vm *vm, t_cursor *c, t_arg *argument)
 		if (argument[i].type == 2)
 			value[i] = argument[i].value;
 		if (argument[i].type == 4)
-		{
-			val = swap_32(ARENA[c->position + argument[i].value % IDX_MOD]);
-			ft_memcpy(&value[i], &val, 4);		
-		}
+			value[i] = get_bytes(ARENA, c->position + argument[i].value % IDX_MOD, 4);
 		i++;
 	}
 	val = value[0] | value[1];
@@ -165,11 +162,11 @@ void	xor(t_vm *vm, t_cursor *c, t_arg *argument)
 		if (argument[i].type == 2)
 			value[i] = argument[i].value;
 		if (argument[i].type == 4)
-			ft_memcpy(&value[i], &ARENA[c->position + argument[i].value % IDX_MOD], 4);		
+			value[i] = get_bytes(ARENA, c->position + argument[i].value % IDX_MOD, 4);
 		i++;
 	}
 	val = value[0] ^ value[1];
-	ft_memcpy(&c->registry[argument[2].value - 1], &val, 4);
+	c->registry[argument[2].value - 1] = val;
 	if (val == 0)
 		c->carry = 1;
 	else
@@ -192,12 +189,9 @@ void	ldi(t_vm *vm, t_cursor *c, t_arg *argument)
 	int i;
 
 	i = 0;
-	ft_printf("hierzo 22xd \n");
 
 	while (i < 2)
 	{
-		ft_printf("hierzo xd \n");
-
 		if (argument[i].type == 1)
 			value[i] = c->registry[argument[i].value - 1];
 		if (argument[i].type == 2)
@@ -207,17 +201,88 @@ void	ldi(t_vm *vm, t_cursor *c, t_arg *argument)
 			// value[i] = get_4bytes(&ARENA[c->position + argument[i].value % IDX_MOD]);
 		i++;
 	}
-	ft_printf("hierzo xd \n");
 	c->registry[argument[2].value - 1] = get_bytes(ARENA, c->position + (value[0] + value[1]) % IDX_MOD, 4);
-	// c->registry[argument[2].value - 1] = get_4bytes(&ARENA[c->position + (value[0] + value[1]) % IDX_MOD]);
-
 }
 
-/*
-** GOEIEMORGE ROBBE!!! 
-** NIEUWE FUNCTIE ALERT: get_bytes(ARENA, int : current idx, int : amount of bytes you want to get) -> return(int : little endian AKA niks aan doen)
-** LUV
-*/
+void	sti(t_vm *vm, t_cursor *c, t_arg *argument)
+{
+	int value[3];
+	int i;
+
+	i = 0;
+
+	while (i < 3)
+	{
+		if (argument[i].type == 1)
+			value[i] = c->registry[argument[i].value - 1];
+		if (argument[i].type == 2)
+			value[i] = argument[i].value;
+		if (argument[i].type == 4)
+			value[i] = get_bytes(ARENA, c->position + argument[i].value % IDX_MOD, 4);
+		i++;
+	}
+	value[0] = swap_32(value[0]);
+	put_value(ARENA, c->position + (value[1] + value[2]) % IDX_MOD, &value[0]);
+}
+
+void	lld(t_vm *vm, t_cursor *c, t_arg *argument)
+{
+	t_arg arg1;
+	t_arg arg2;
+
+	arg1 = argument[0];
+	arg2 = argument[1];
+	if (arg1.type == 4)
+		c->registry[arg2.value - 1] = get_bytes(ARENA, (c->position + arg1.value) % MEM_SIZE, 4);
+		// c->registry[arg2.value - 1] = get_4bytes(&ARENA[c->position + arg1.value % IDX_MOD]);
+	else if (arg1.type == 2)
+		c->registry[arg2.value - 1] = arg1.value;
+	if (c->registry[arg2.value - 1] == 0)
+			c->carry = 1;
+		else
+			c->carry = 0;
+}
+
+void	lldi(t_vm *vm, t_cursor *c, t_arg *argument)
+{
+	int value[2];
+	int i;
+
+	i = 0;
+
+	while (i < 2)
+	{
+		if (argument[i].type == 1)
+			value[i] = c->registry[argument[i].value - 1];
+		if (argument[i].type == 2)
+			value[i] = argument[i].value;
+		if (argument[i].type == 4)
+			value[i] = get_bytes(ARENA, c->position + argument[i].value % IDX_MOD, 4);
+			// value[i] = get_4bytes(&ARENA[c->position + argument[i].value % IDX_MOD]);
+		i++;
+	}
+	c->registry[argument[2].value - 1] = get_bytes(ARENA, (c->position + value[0] + value[1]) % MEM_SIZE, 4);
+}
+
+void	aff(t_vm *vm, t_cursor *c, t_arg *argument)
+{
+	char c;
+
+	c = c->registry[argument[0].value - 1];
+	write(1, &c, 1);
+}
+
+void	ftfork(t_vm *vm, t_cursor *c, t_arg *argument)
+{
+	// new cursor
+	// newcursor->pos = argument[0].value % IDX_MOD;
+}
+
+void	lfork(t_vm *vm, t_cursor *c, t_arg *argument)
+{
+	// new cursor
+	// newcursor->pos = argument[0].value % MEM_SIZE;
+}
 
 void	do_op(t_vm *vm, t_cursor *cursor, t_arg *args, int size)
 {
@@ -244,18 +309,18 @@ void	do_op(t_vm *vm, t_cursor *cursor, t_arg *args, int size)
 		zjmp(vm, cursor, args);
 	else if (opcode == 10)
 		ldi(vm, cursor, args);
-	// else if (opcode == 11)
-	// 	sti(vm, cursor, args);
-	// else if (opcode == 12)
-	// 	ftfork(vm, cursor, args);
-	// else if (opcode == 13)
-	// 	lld(vm, cursor, args);
-	// else if (opcode == 14)
-	// 	lldi(vm, cursor, args);
-	// else if (opcode == 15)
-	// 	lfork(vm, cursor, args);
-	// else if (opcode == 16)
-	// 	aff(vm, cursor, args);
+	else if (opcode == 11)
+		sti(vm, cursor, args);
+	else if (opcode == 12)
+		ftfork(vm, cursor, args);
+	else if (opcode == 13)
+		lld(vm, cursor, args);
+	else if (opcode == 14)
+		lldi(vm, cursor, args);
+	else if (opcode == 15)
+		lfork(vm, cursor, args);
+	else if (opcode == 16)
+		aff(vm, cursor, args);
 	cursor->position += size;
 	cursor->moved = true;
 }
